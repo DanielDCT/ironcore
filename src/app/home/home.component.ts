1a3916402767f0c31a9bd0e19684c8ee9c2f7756
Daniel <calderondanieldavid@gmail.com>
2023-08-04 17:04:52 -0500

    Quinto commit - Correcciones meta tags - Agregado de google analytics y verification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +6,6 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(private meta:Meta) {
-    meta.updateTag({name:'description', content:'Iron Core ofrece entrenamiento personal de alta calidad en Villeta, Cundinamarca. Descubre nuestros planes y comentarios de nuestros clientes satisfechos.'})
-    meta.updateTag({name:'copyright', content:'Copyright - Iron Core - Edgar Enrique Calderón Ramírez'})
-    meta.updateTag({name: 'author', content: 'Daniel David Calderón Tinjaca'});
-    meta.updateTag({name:'robots', content:'index, follow'})
-    meta.updateTag({name: 'language', content: 'es'});
-  }
 
   // Se agrega el siguiente código para realizar el desplazamiento suave al elemento (SECCION) correspondiente
   scrollToComponent(elementId: string): void {
